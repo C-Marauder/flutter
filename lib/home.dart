df1d9d408d6138b00bfe735ea4306d076d864118
xqy666666 <17775621462@163.com>
2019-03-11 21:24:28 +0800

    提交

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget{
@@ -11,9 +10,30 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  int _currentTab =0;
   @override
   Widget build(BuildContext context) {
-    return Card(color: Colors.amber,);
+    return Column(children: <Widget>[
+      TabBarView(children:[
+        Column(children: <Widget>[
+          Icon(Icons.security),
+          Text("社保")
+        ],)
+
+      ])
+    ],);
   }
 
 }
+class TabData {
+  const TabData({this.title,this.icon});
+
+  final String title;
+  final IconData icon;
+}
+const List<TabData> tabs = const <TabData>[
+  const TabData(title: '社保', icon: Icons.security),
+  const TabData(title: '信用卡', icon: Icons.credit_card),
+  const TabData(title: '公积金', icon: Icons.account_balance)
+
+];
